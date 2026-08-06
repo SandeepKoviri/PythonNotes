d9c7f1e1982363f228279d17613172b1efa61438
SandeepKoviri <198357274+SandeepKoviri@users.noreply.github.com>
2026-08-06 15:57:07 +0530

    today's  notes and tilldate notes update

--- PythonNotesTilldate.py
+++ PythonNotesTilldate.py
@@ -1801,9 +1801,509 @@ print(next(b))
 print(next(b))
 print(next(b))
 '''
+'''
+--------------
+---day 15-----
+--08/04/26---
+-------------
+module
+------
+--> Module are teh python  code which is saved in (.py) extention
+--> which contains functions , variables and classes
+
+type of module
+1. Built-in module
+---> the built-in module are the module which is already designed with  python when we install python in our system
+eg:
+math, sys, os, random, datetime, calendar, json, time, statistics
+
+2. user-defined module
+---> the user-defined module are the module which is created by the user/ programmer to use in their program
+---> syntax: import(keyword) module_name
+
+eg :
+fisrt_m.py
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+next we can use this module in our program by importing it
+eg:
+import first_M 
+
+print(first_M.add(10,20))
+print(first_M.sub(10,20))
+print(first_M.mul(10,20))
+print(first_M.div(10,20))
+
+-importing with alias name
+--------------------------
+---> we can import the module with alias name to use in our program
+---> after importing the module with alias name ,we have to call the function with alias name in the code
+---> syntax: import module_name as alias_name
+eg:
+import first_M as fm
+
+print(fm.add(10,20))
+print(fm.sub(10,20))
+print(fm.mul(10,20))
+print(fm.div(10,20))
+
+-importing only needed function from module
+-----------------------------------
+---> we can import only the specific function from the module that we need in our program
+
+from first_M import add,sub,mul,div
+
+
+print(add(10,20))
+print(sub(10,20))
+print(mul(10,20))
+print(div(10,20))
+
+-importing all function from module
+-----------------------------------
+---> we can import all the function from the module we have to use (* ) in the import statement
+--> syntax: from module_name import *
+eg:
+
+from first_M import *
+
+print(add(10,20))
+print(sub(10,20))
+print(mul(10,20))
+print(div(10,20))
+
+-
+
+name = "sandeep"
+
+def get_name():
+    print(f"my name is {name}")
+
+we import the module in our program we can use the function get_name() in our program
+eg:
+from first_M import get_name
+get_name()
+
+-random module
+-----------------
+-->
+import random as r
+
+print(r.randint(1000, 9999 ))
+
+-math module
+import math as m
+
+print(m.sqrt(25))
+sand = {
+    'name' : 'sandeep',
+    'atm_pin' : '1234'
+}
+import random
+rem = 3
+while rem > 0:
+    pin = int(input("Enter The pin :"))
+    if pin == sand['atm_pin']:
+        otp = random.randint(1000,9999) 
+        print(otp)
+        otp_ = int(input("Enter OTP:"))
+        if otp == otp_:
+            opt= int(input("Enter choice:"))
+        else:
+            print("Incorrect Otp:")
+            exit()
+    else:
+        rem -= 1
+        if rem > 0:
+            print(f'Incorrecty pin {rem} attempts left')
+        else:
+            print("card block"
+'''
 
 
 
 
 
+
+'''
+------day16------
+----------------
+---5/08/26-----
+----------------
+math
+---
+---> math module used to work on mathematicle functionality
+
+floor
+-----
+it will round-down to the near value
+eg:
+import math
+print(math.floor(3.14))
+
+ceil
+---
+--> it will round-up to near value
+eg:
+import math
+print(math.ceil(3.14))
+
+gcd
+----
+--> it will find gcd value
+eg:
+import math
+print(math.gcd(24,32))
+
+lcm:
+----
+--> it will find the value of lcm
+eg:
+import math
+print(math.lcm(24,32))
+
+sqrt()
+-----
+--> we will get the sqrt of the value
+eg:
+import math
+print(math.sqrt(25))
+
+factorial()
+----------
+--> it will give the value of factorial
+
+eg:
+import math
+print(math.factorial(5))
+
+import math
+print(math.log(5,10))
+print(math.cos(math.pi/2))
+print(math.pi)
+
+
+random -module
+-------------
+--> the random module use to get random number
+-randint
+------
+--> it use to generate random number based on the range
+
+import random
+
+print(random.randint(1,9))
+
+-choice()
+---------
+---> it will pick the random value from the given data
+-shuffle()
+--------
+---> we can shuffel the data
+
+eg:
+import random
+print(random.randint(1,9))
+
+colour = ['red','yello','blue','green']
+print(random.choice(colour))
+random.shuffle(colour)
+print(colour)
+
+-uniform()
+---------
+---> it gives decimal values from the range
+
+eg:
+import random
+
+print(random.uniform(1,10))
+
+
+
+sys - module
+-----------
+-version
+----------
+--> the version of the python interperater
+-path
+-----
+--> .py path we will get by this function
+
+eg:
+import sys
+
+print(sys.version)
+print(sys.path)
+
+
+exit
+----
+--> this function will exit from the program
+eg
+--
+import sys
+print(sys.exit())
+
+platform
+--------
+--> it will give the python run platform
+
+eg
+--
+import sys
+print(sys.platform)
+
+o/p:
+win32
+
+argv
+----
+--> it will give the current file run path
+
+eg
+--
+import sys
+print(sys.argv)
+
+o/p:
+['C:\\Users\\rajana yaswanth\\Downloads\\python\\module.py']
+
+datetime
+--------
+--> used to work with date and time
+
+now
+---
+--> it will give the today time + date
+
+eg
+--
+from datetime import datetime, date, time
+
+print(datetime.now())
+print(datetime.today())
+
+o/p:
+2026-08-05 14:50:39.607454
+2026-08-05 14:50:39.655701
+
+
+from datetime import datetime
+now = datetime.now()
+print(now.strftime('%Y-%m-%d'))
+print(now.strftime('%A'))
+print(now.strftime('%B'))
+print(now.strftime('%H:%M:%S'))
+
+o/p:
+2026-08-05
+Wednesday
+August
+14:58:05
+
+%Y --> will get the year
+%m --> will get the month
+%d --> will get the day
+%H --> will get the hour
+%M --> will get the minutes
+%S --> will get the seconds
+%A --> current day
+%B --> current month
+
+collections
+-----------
+-->the collections module will provide container type data which is more powerful than built-in data types (dict, list, tuple)
+
+import collections
+
+data = ['yash', 'raj', 'woxsen', 'uni','yash']
+print(collections.Counter(data))
+
+deque
+-----
+--> used to work with list
+
+eg
+--
+from collections import deque
+
+how = deque([7,8,9])
+how.appendleft(6)
+print(how)
+
+o/p:
+deque([6, 7, 8, 9])
+
+extend
+------
+
+from collections import deque
+how = ([1,2,3])
+how.extend([4,5,6,7])
+print(how)
+
+o/p:
+v[1, 2, 3, 4, 5, 6, 7]
+
+pop
+---
+
+from collections import deque
+how = deque([1,2,3])
+how.pop()
+print(how)
+
+o/p:
+deque([1, 2])
+
+namedtuple
+----------
+
+from collections import namedtuple
+data = namedtuple("stu",('name','age'))
+print(data('yash','21'))
+
+o/p:
+stu(name='yash', age='21')
+
+itertools
+---------
+
+count
+-----
+
+from itertools import count
+
+c = count(76)
+for j in range(7):
+    print(next(c))
+
+o/p;
+76
+77
+78
+79
+80
+81
+82
+
+repeat
+------
+
+import itertools
+for j in itertools.repeat('yash',7):
+    print(j)
+
+o/p:
+yash
+yash
+yash
+yash
+yash
+yash
+yash
+
+
+from itertools import permutations
+
+data = permutations([1,2,3],2)
+print(list(data))
+
+o/p:
+[(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)]
+
+from itertools import permutations, combinations
+
+data = permutations([1,2,3],2)
+print(list(data))
+
+any_ = combinations([1,2,3],2)
+print(list(any_))
+
+0/p:
+[(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)]
+[(1, 2), (1, 3), (2, 3)]
+
+import platform
+print(platform.python_version())
+print(platform.python_compiler())
+print(platform.machine())
+print(platform.processor())
+
+----------
+'''
+'''
+---------------
+-----day17-----
+---6/08/26----
+asscii_letters --> this string module function that can give upper and lower letters
+digits --> string module function that can give numbers (0-9)
+punctuation --> this string module function can give us special characters(!@#$&)
+
+import random
+import string
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+
+o/p:
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+
+import random
+import string
+
+letters = string.ascii_letters
+digits = string.digits
+special_chars = '@#$*'
+
+all_chars = letters + digits + special_chars
+
+password = ''
+for i in range(5):
+    password += random.choice(all_chars)
+
+print(password)
+
+bank_balance = 7777
+
+from datetime import datetime
+import sys
+now = datetime.now()
+
+while True:
+    print("--------Welcome to SBI ATM----------")
+    user_opt = int(input("\n1.withdraw \n2.deposit \n3.check balance \n4.exit"))
+    if user_opt == 1:
+        withdraw_money = int(input("enter the money"))
+        if withdraw_money > bank_balance:
+            bank_balance -= withdraw_money
+            print(f'remaining money {bank_balance} {now.strftime("%H:%M %Y-%m-%d")}')
+        else:
+            print('insufficient money')
+    elif user_opt == 2:
+        deposit_m = int(input("enter the money you want to deposit"))
+        bank_balance += deposit_m
+        print(f' money added successfully: {bank_balance} {now.strftime("%H:%M %Y-%m-%d")}')
+    elif user_opt == 3:
+        print(f'avaiable balance : {bank_balance} {now.strftime("%H:%M %Y-%m-%d")}')
+    elif user_opt == 4:
+        sys.exit()
+
+    else:
+        print("incorrect choice")
+        print("thank you for visiting the ATM")
+        sys.exit()
+'''
 
---
+++ day17/notes.py
@@ -0,0 +1,67 @@
+'''
+---------------
+-----day17-----
+---6/08/26----
+asscii_letters --> this string module function that can give upper and lower letters
+digits --> string module function that can give numbers (0-9)
+punctuation --> this string module function can give us special characters(!@#$&)
+
+import random
+import string
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+
+o/p:
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+
+import random
+import string
+
+letters = string.ascii_letters
+digits = string.digits
+special_chars = '@#$*'
+
+all_chars = letters + digits + special_chars
+
+password = ''
+for i in range(5):
+    password += random.choice(all_chars)
+
+print(password)
+
+bank_balance = 7777
+
+from datetime import datetime
+import sys
+now = datetime.now()
+
+while True:
+    print("--------Welcome to SBI ATM----------")
+    user_opt = int(input("\n1.withdraw \n2.deposit \n3.check balance \n4.exit"))
+    if user_opt == 1:
+        withdraw_money = int(input("enter the money"))
+        if withdraw_money > bank_balance:
+            bank_balance -= withdraw_money
+            print(f'remaining money {bank_balance} {now.strftime("%H:%M %Y-%m-%d")}')
+        else:
+            print('insufficient money')
+    elif user_opt == 2:
+        deposit_m = int(input("enter the money you want to deposit"))
+        bank_balance += deposit_m
+        print(f' money added successfully: {bank_balance} {now.strftime("%H:%M %Y-%m-%d")}')
+    elif user_opt == 3:
+        print(f'avaiable balance : {bank_balance} {now.strftime("%H:%M %Y-%m-%d")}')
+    elif user_opt == 4:
+        sys.exit()
+
+    else:
+        print("incorrect choice")
+        print("thank you for visiting the ATM")
+        sys.exit()
+'''
+
