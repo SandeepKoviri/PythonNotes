1dbad1e76f1ee4edc336ecf6cfc3bae672d0ae5c
SandeepKoviri <198357274+SandeepKoviri@users.noreply.github.com>
2026-08-06 16:00:49 +0530

    created a game for chose a no.

---
+++ projects/proj4_chGame.py
@@ -0,0 +1,13 @@
+import random
+
+comp = random.randint(0,10)
+print("----------------------------------------------------------")
+print("---------------------Gess The Number----------------------")
+print("----------------------------------------------------------")
+user = int(input("Enter the number(0-10): "))
+
+if comp == user:
+    
+    print(f"{comp} and {user}\n U won!..........")
+else:
+    print(f"{comp} and {user}\nU lost \n/'better luck next time.../'")
